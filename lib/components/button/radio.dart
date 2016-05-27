c8ad4c508a8c169742f635b2de42eeed3118e8fc
Adao Jr <itsjunnior@gmail.com>
2016-05-27 18:01:35 -0300

    dart fmt

--- lib/components/button/radio.dart
+++ lib/components/button/radio.dart
@@ -1,32 +1,34 @@
 import "package:angular2/angular2.dart";
 
-@Directive(selector: "bs-radio-button",
-    host: const {"[class.active]" : "active"})
+@Directive(
+    selector: "bs-radio-button", host: const {"[class.active]": "active"})
 class RadioButton extends DefaultValueAccessor {
-  RadioButton(this.ngModel, Renderer renderer, ElementRef elementRef)
-      : super (renderer, elementRef) {
-    ngModel.valueAccessor = this;
-  }
-
   /// handles the selected value of the button
   NgModel ngModel;
 
   /// option to be selected
-  @Input() var option;
+  @Input()
+  var option;
 
   /// if `true` the button group can be unchecked
-  @Input() bool uncheckable = true;
+  @Input()
+  bool uncheckable = true;
 
   /// provide the state of the button
   bool get active => option == _value;
 
   var _value;
 
+  RadioButton(this.ngModel, Renderer renderer, ElementRef elementRef)
+      : super(renderer, elementRef) {
+    ngModel.valueAccessor = this;
+  }
+
   /// listen when the value of the button has changed
   @override
   writeValue(value) async {
-      _value = value;
-      super.writeValue(value);
+    _value = value;
+    super.writeValue(value);
   }
 
   /// listen on the click event of the button
--- lib/components/button/toggle.dart
+++ lib/components/button/toggle.dart
@@ -1,31 +1,32 @@
 import "package:angular2/angular2.dart";
 
-
 @Directive(
-    selector: "bs-toggle-button",
-    host: const {"[class.active]" : "active"}
-)
+    selector: "bs-toggle-button", host: const {"[class.active]": "active"})
 class ToggleButton extends DefaultValueAccessor {
   NgModel ngModel;
 
-  ToggleButton(this.ngModel, Renderer renderer, ElementRef elementRef) : super(renderer, elementRef) {
-    ngModel.valueAccessor = this;
-  }
   /// if it is equals to the [ngModel] value then the checkbox is going to be active
-  @Input() dynamic trueValue = true;
+  @Input()
+  dynamic trueValue = true;
 
   /// if it is not equals to the [ngModel] value then the checkbox is not going to be active
-  @Input() dynamic falseValue = false;
+  @Input()
+  dynamic falseValue = false;
 
   var _value;
 
   /// active status of the button
   bool get active => trueValue == _value;
 
+  ToggleButton(this.ngModel, Renderer renderer, ElementRef elementRef)
+      : super(renderer, elementRef) {
+    ngModel.valueAccessor = this;
+  }
+
   /// this function is fired whenever a new write is done to the [ngModel]
   writeValue(value) async {
-      _value = value;
-      super.writeValue(_value);
+    _value = value;
+    super.writeValue(_value);
   }
 
   /// toggles the state of the [active] attribute
@@ -36,7 +37,5 @@ class ToggleButton extends DefaultValueAccessor {
 
   /// Listens on the click event of the button
   @HostListener('click')
-  onClick() {
-    toggle(!active);
-  }
+  onClick() => toggle(!active);
 }
