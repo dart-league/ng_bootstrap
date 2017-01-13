a0f781b7a03dfd359a0b47fce49d2ed8fdb304cf
Luis Vargas <luisvargastijerino@gmail.com>
2017-01-13 00:12:28 -0500

    correct `sass` grinder task to use dart-sass `render` function with `SyncPackageResolver`

--- tool/grind.dart
+++ tool/grind.dart
@@ -1,11 +1,12 @@
 import 'dart:io';
 import 'package:grinder/grinder.dart';
 import 'package:sass/sass.dart';
+import 'package:sass/src/sync_package_resolver/sync_package_resolver.dart';
 
 main(args) => grind(args);
 
 @DefaultTask('Build Scss Files')
 sass() async {
-  var css = await render('lib/all.scss');
+  var css = render('lib/all.scss', packageResolver: await SyncPackageResolver.current);
   new File('lib/all.css').writeAsStringSync(css);
 }
