2c0478c58de8beb06fdd5be47940a56b002c97f5
Luis Vargas <luisbigvargas@gmail.com>
2021-01-10 14:48:02 -0500

    fix demos' code urls

--- demo/web/components/demo_section.dart
+++ demo/web/components/demo_section.dart
@@ -23,7 +23,7 @@ class DemoSection implements OnInit {
   void ngOnInit() async {
     nameTN = toTableName(name);
     var docPathNameTN = 'components_$nameTN\_$nameTN';
-    var rawMasterUrl = 'https://raw.githubusercontent.com/dart-league/ng_bootstrap/develop/demo';
+    var rawMasterUrl = 'https://raw.githubusercontent.com/dart-league/ng_bootstrap/master/demo';
     var componentsUrl = '$rawMasterUrl/web/components';
     var _docPath = docPath ?? docPathNameTN;
     docUrl = 'https://www.dartdocs.org/documentation/ng_bootstrap/latest/$_docPath/$_docPath-library.html';
